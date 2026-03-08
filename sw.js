8c0796dc2a79790bcd3c3c4e3b103fac7f544132
alessandrociapina-cpu <alessandro.ciapina@gmail.com>
2026-03-08 16:50:27 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ovms-app-v5'; // Força a atualização
+const CACHE_NAME = 'ovms-app-v6'; 
 const urlsToCache = [
   './',
   './index.html',
@@ -12,24 +12,16 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
-  // Oculta caches antigos e instala o novo
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('activate', event => {
-  // Limpa o lixo das versões antigas (v1)
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+          if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
         })
       );
     })
@@ -38,12 +30,5 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
-
-
-

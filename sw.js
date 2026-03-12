23360a444fb0db9a928afaa80955100246e853ed
alessandrociapina-cpu <alessandro.ciapina@gmail.com>
2026-03-12 08:21:01 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'ovms-app-v29'; 
+const CACHE_NAME = 'ovms-app-v30'; 
 
 const urlsToCache = [
   './',
   './index.html',
   './documentacao.html',
-  './style.css?v=29',
-  './script.js?v=29',
+  './style.css?v=30',
+  './script.js?v=30',
   './manifest.json',
   './sabesp-logo.png',
   'https://cdn.jsdelivr.net/npm/exif-js',
@@ -31,15 +31,12 @@ self.addEventListener('activate', event => {
   self.clients.claim();
 });
 
-// O Motor Definitivo: 100% NETWORK FIRST para todos os ficheiros!
 self.addEventListener('fetch', event => {
-  // Ignora requisições que não sejam GET (segurança padrão)
   if (event.request.method !== 'GET') return;
 
   event.respondWith(
     fetch(event.request)
       .then(response => {
-        // Se a internet funcionou, guarda a versão mais nova escondido no cache
         const clone = response.clone();
         caches.open(CACHE_NAME).then(cache => {
           cache.put(event.request, clone);
@@ -47,7 +44,6 @@ self.addEventListener('fetch', event => {
         return response;
       })
       .catch(() => {
-        // Se falhou (telemóvel sem internet/offline na obra), usa o que tem no cache
         return caches.match(event.request);
       })
   );
